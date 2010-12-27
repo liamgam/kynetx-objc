0c9bccb8c315a99934614faa59099bcf740a138c
liamgam <liam.liamgamble@gmail.com>
2010-12-27 12:40:31 -0700

    Addition of some files.

---
+++ appcontroller.h
@@ -0,0 +1,16 @@
+//
+//  appcontroller.h
+//  kynetx-desktop
+//
+//  Created by Alex  on 12/27/10.
+//  Copyright 2010 Kynetx. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface appcontroller : NSObject {
+
+}
+
+@end
---
+++ appcontroller.m
@@ -0,0 +1,14 @@
+//
+//  appcontroller.m
+//  kynetx-desktop
+//
+//  Created by Alex  on 12/27/10.
+//  Copyright 2010 Kynetx. All rights reserved.
+//
+
+#import "appcontroller.h"
+
+
+@implementation appcontroller
+
+@end
