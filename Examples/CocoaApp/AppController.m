45c7a284b1360018aef9c868de639e70cb81afed
liamgam <liam.liamgamble@gmail.com>
2011-01-05 13:12:24 -0700

    Revert "moving to a delegate model." Badneess! This reverts commit c165c6b390ec8cb2027abdaeb0913e9d19cb6959.

--- Examples/CocoaApp/AppController.m
+++ Examples/CocoaApp/AppController.m
@@ -14,9 +14,10 @@
 @synthesize app;
 
 -(IBAction) sendTestKynetx:(id) sender {
-	[self setApp:[[[Kynetx alloc] initWithAppID:[appIDField stringValue] eventDomain:nil] autorelease]];
+	[self setApp:[[[Kynetx alloc] initWithAppID:[appIDField stringValue]] autorelease]];
 	NSDictionary* urlParams = [NSDictionary dictionaryWithObjectsAndKeys:@"bar", @"foo", @"whiz", @"cheese", nil];
-	[app signal:@"jump_for_joy" params:urlParams];
+	NSArray* directives = [app raiseEvent:@"jump_for_joy" params:urlParams];
+	NSLog(@"%@", directives);
 }
 
 -(IBAction) appIDVal:(id) sender {
